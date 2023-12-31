4eba83d69146efb9005e1e4afb6bcc57d106a2ec
SmittyB00p <tsmith.smith391@gmail.com>
2023-12-30 19:58:49 -0600

    added a simple function to the index.py file

--- import folder/index.py
+++ import folder/index.py
@@ -1,1 +1,17 @@
-print('Hello World!')
+#function that takes in a number greater than 1 and makes it into a list of that length
+def create_num_list(num=1):
+    #returns 0 if the input was 0
+    if num == 0:
+        return num
+    
+    num_list = []
+
+    #while the number is greater than 0 the while loop will add the number to the num_list, decrease the number and do that until it is less than 0
+    while num > 0:
+        num_list.append(num)
+        num -= 1
+
+    return num_list
+
+
+print(create_num_list(10))
